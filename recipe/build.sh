80ec5334c313cf7a0d5b2669d21c4fe6b7c831b1
Ray Donnelly <mingw.android@gmail.com>
2018-01-02 13:33:36 +0000

    Use BUILD_PREFIX for CDT java packages

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,16 @@
 
 # Find Java in the compiler's sysroot
 if [[ ${HOST} =~ .*linux.* ]]; then
-  export JAVAC=$(find ${PREFIX}/${HOST}/sysroot -path "*/bin/javac")
-  export JAVAH=$(find ${PREFIX}/${HOST}/sysroot -path "*/bin/javah")
+  export JAVAC=$(find ${BUILD_PREFIX}/${HOST}/sysroot -path "*/bin/javac")
+  export JAVAH=$(find ${BUILD_PREFIX}/${HOST}/sysroot -path "*/bin/javah")
   export JAVA=${JAVAC%?}
-  export JAR=$(find ${PREFIX}/${HOST}/sysroot -path "*/bin/jar")
+  export JAR=$(find ${BUILD_PREFIX}/${HOST}/sysroot -path "*/bin/jar")
   # Hack so that sysroot/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.131.x86_64/jre/lib/amd64/libnio.so
   # finds sysroot/usr/lib{64}/libgconf-2.so.4 (horrible..)
   if [[ ${HOST} =~ i686.* ]]; then
-    export LD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/${HOST}/sysroot/usr/lib
+    export LD_LIBRARY_PATH=${PREFIX}/lib:${BUILD_PREFIX}/${HOST}/sysroot/usr/lib
   else
-    export LD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/${HOST}/sysroot/usr/lib64
+    export LD_LIBRARY_PATH=${PREFIX}/lib:${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64
   fi
 fi
 
