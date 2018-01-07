d68706c0b3184619358b7af2afee1b29dbd001b6
Ray Donnelly <mingw.android@gmail.com>
2018-01-07 13:14:17 +0000

    Remove debugging stuff (but keep rjava-dump-details)
    .. please do not squash this away, I will likely need it again.

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,35 +23,8 @@ chmod +x $PWD/java_reconf
 JAVA_LD_LIBRARY_PATH=$($R CMD javareconf -e $PWD/java_reconf | tail -n1)
 echo "JAVA_LD_LIBRARY_PATH as determined by R CMD javareconf: $JAVA_LD_LIBRARY_PATH"
 
-if [[ $(uname) == Darwin ]]; then
-  EREGEX=-e
-fi
-
-cp ${PREFIX}/lib/R/bin/javareconf ${RECIPE_DIR}/javareconf.${r_base}
 # Now re-configure to set the correct values.
-if ! grep 'set -x' ${PREFIX}/lib/R/bin/javareconf > /dev/null; then
-  sed -i'' ${EREGEX} '1i\'$'\n''set -x\'$'\n' ${PREFIX}/lib/R/bin/javareconf
-fi
-R CMD javareconf 2>&1 | tee ${RECIPE_DIR}/R-CMD-javareconf.log
-if [[ $? == 0 ]]; then
-  echo working > ${DEST}/R-CMD-javareconf-status.log
-else
-  echo broken > ${DEST}/R-CMD-javareconf-status.log
-fi
-cp ${RECIPE_DIR}/javareconf.3.4.3.WIP ${PREFIX}/lib/R/bin/javareconf
-R CMD javareconf 2>&1 | tee ${RECIPE_DIR}/R-CMD-javareconf-WIP.log
-if [[ $? != 0 ]]; then
-  OK_R_CMD_JAVARECONF=no
-  echo working > ${DEST}/R-CMD-javareconf-WIP-status.log
-else
-  OK_R_CMD_JAVARECONF=yes
-  echo broken > ${DEST}/R-CMD-javareconf-WIP-status.log
-fi
-cp ${RECIPE_DIR}/javareconf.${r_base} ${PREFIX}/lib/R/bin/javareconf
-if [[ ${OK_R_CMD_JAVARECONF} == no ]]; then
-  echo "R CMD javareconf failed"
-  exit 1
-fi
+R CMD javareconf
 
 if [[ ${HOST} =~ .*linux.* ]]; then
   export LD_LIBRARY_PATH=${JAVA_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
