8686faa867733512975db525dc99ff37850f2b25
rvalieris <rvalieris@users.noreply.github.com>
2018-01-24 14:31:48 -0200

    add patch to help find libjvm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [[ "$(uname)" == "Linux" ]]; then
+  export LD_LIBRARY_PATH=${JAVA_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
+elif [[ "$(uname)" == "Darwin" ]]; then
+  export DYLD_FALLBACK_LIBRARY_PATH=${JAVA_LD_LIBRARY_PATH}:${DYLD_FALLBACK_LIBRARY_PATH}
+fi
+
 $R CMD INSTALL --build .
