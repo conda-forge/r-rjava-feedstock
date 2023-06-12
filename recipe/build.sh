16d1e1d596f5b9f853002f3a028c6344085a6346
Uwe L. Korn <uwe.korn@quantco.com>
2023-06-12 15:56:21 +0200

    Patch for cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -exuo pipefail
+
 export DISABLE_AUTOBREW=1
 
 # ${R_ARGS} necessary to support cross-compilation
 export PATH=$PATH:$JAVA_HOME/bin
-${R} CMD javareconf
+
+autoreconf --install
+autoconf
+pushd jri
+  autoconf
+popd
 ${R} CMD INSTALL --build . ${R_ARGS}
